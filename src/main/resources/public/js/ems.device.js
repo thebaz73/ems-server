b22f516f4115987d5e38c1f582a58518c4a463ab
Marco Bazzoni <bazzoni.marco@gmail.com>
2014-09-23 12:21:46 +0200

    Generic bug fix

--- src/main/resources/public/js/ems.device.js
+++ src/main/resources/public/js/ems.device.js
@@ -3,7 +3,7 @@
  */
 var paths = [];
 var selectors = [];
-var deviceData = {
+var deviceEventsData = {
     uri: "",
     data: {},
     page: 0,
@@ -26,20 +26,29 @@ $(document).ready(function () {
     updateData();
 });
 
+/**
+ * Recurrent function to update all data
+ */
 function updateData() {
     var request = "/inventory/device/" + deviceId;
+    var $devicePanel = $("#devicePanel");
     $.get(request, function(device) {
-        $("#devicePanel").removeClass();
-        $("#devicePanel").addClass("panel");
-        addStatusClass($("#devicePanel"), device.driver.status);
+        $devicePanel.removeClass();
+        $devicePanel.addClass("panel");
+        addStatusClass($devicePanel, device.driver.status);
         assignValues(device.driver, selectors, paths);
-        //console.log(JSON.stringify(device));
     });
-    loadDevicePagedData("/inventory/events/"+deviceId, "/template/deviceLogPage");
+    doLoad("/inventory/events/"+deviceId, "/template/deviceLogPage");
 
     setTimeout(updateData, updateFrequency);
 }
 
+/**
+ * Sets a proper class according to device status
+ *
+ * @param element panel element
+ * @param status device status
+ */
 function addStatusClass(element, status) {
     if(status === 'OK') {
         element.addClass("panel-success");
@@ -64,33 +73,57 @@ function addStatusClass(element, status) {
  * @param pageSize page size
  */
 function loadDevicePagedData(uri, template, page, pageSize) {
-    if(page) {
-        deviceData.page = page;
-    }
-    if(pageSize) {
-        deviceData.pageSize = pageSize;
-    }
-    var request = uri + "?page=" + deviceData.page + "&pageSize=" + deviceData.pageSize;
+    deviceEventsData.page = page;
+    deviceEventsData.pageSize = pageSize;
+    doLoad(uri, template);
+}
+
+/**
+ * Loads device related data
+ *
+ * @param uri base uri
+ * @param template rendering template
+ */
+function doLoad(uri, template) {
+    var request = uri + "?page=" + deviceEventsData.page + "&pageSize=" + deviceEventsData.pageSize;
     var response = $.getJSON(request);
     response.done(function(data, deviceId) {
-        deviceData.uri = uri;
-        deviceData.data = data;
+        deviceEventsData.uri = uri;
+        deviceEventsData.data = data;
         $.get(template, function (template) {
             var t = Handlebars.compile(template);
-            $('#log-page').html(t(deviceData));
+            $('#log-page').html(t(deviceEventsData));
             $("[data-toggle='popover']").popover();
         });
     });
+
 }
 
+/**
+ * Traverses a JSON object representing a driver schema
+ * in order to extract a path of elementary properties
+ * and a relative DOM selector
+ *
+ * @param obj driver schema
+ * @param pathCursor path cursor
+ * @param paths array of paths
+ * @param selectorCursor selector cursor
+ * @param selectors array of selectors
+ */
 function traverse(obj, pathCursor, paths, selectorCursor, selectors) {
     if(obj.type === 'object') {
         var tmpObj = obj.properties;
         for(var key in tmpObj) {
             if(key !== 'status' && key !== 'type' && key !== 'location') {
                 if(tmpObj[key].type !== 'object' &&  tmpObj[key].type !== 'array') {
-                    paths.push(pathCursor.join('.') + '.' + key);
-                    selectors.push(selectorCursor.join(' ') + ' ' + '*[data-path="'+key+'"]');
+                    if(pathCursor.length > 0) {
+                        paths.push(pathCursor.join('.') + '.' + key);
+                        selectors.push(selectorCursor.join(' ') + ' ' + '*[data-path="'+key+'"]');
+                    }
+                    else {
+                        paths.push(key);
+                        selectors.push('*[data-path="'+key+'"]');
+                    }
                 }
                 else {
                     pathCursor.push(key);
@@ -114,19 +147,34 @@ function traverse(obj, pathCursor, paths, selectorCursor, selectors) {
     }
 }
 
+/**
+ * Follows path along the object to find
+ * the elementary property given by the path
+ *
+ * @param obj driver object
+ * @param path property path
+ * @returns {*} property object
+ */
 function getObjectPath(obj, path) {
     var parts = path.split('.');
     while (parts.length && (obj = obj[parts.shift()]));
     return obj;
 }
 
+/**
+ * Assigns all object property values to DOM selectors
+ *
+ * @param driver driver object
+ * @param selectors DOM selector array
+ * @param paths array of path
+ */
 function assignValues(driver, selectors, paths) {
     for (var i = 0; i < paths.length; i++) {
         var path = paths[i];
         var selector = selectors[i];
         var value = getObjectPath(driver, path);
-        //console.log(value);
-        //console.log($(selector).html());
+        //console.log(path + ": " + value);
+        //console.log(selector + ": " + $(selector).html());
         $(selector).text(value);
     }
 }
